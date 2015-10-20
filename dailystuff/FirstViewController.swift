d9da41a83932f03387b41e992055bdc4127e6694
hoangpham <hoangpham1107@gmail.com>
2015-10-21 01:10:49 +0800

    updater
    update

--- dailystuff/FirstViewController.swift
+++ dailystuff/FirstViewController.swift
@@ -12,10 +12,36 @@ class FirstViewController: UIViewController {
 
 var photoTakingHelper: PhotoHelper?
     
+    @IBOutlet var mainTableView: UITableView!
+    var posts: [Post] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
          self.tabBarController?.delegate = self
+        
+        // 1
+        let followingQuery = PFQuery(className: "Follow")
+        //followingQuery.whereKey("fromUser", equalTo:PFUser.currentUser()!)
+                
+        // 2
+        let postsFromFollowedUsers = Post.query()
+        postsFromFollowedUsers!.whereKey("user", matchesKey: "toUser", inQuery: followingQuery)
+        
+        // 3
+        let postsFromThisUser = Post.query()
+        //postsFromThisUser!.whereKey("user", equalTo: PFUser.currentUser()!)
+        
+        // 4
+        let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
+        // 5
+        //query.includeKey("user")
+        // 6
+        query.orderByDescending("createdAt")
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,5 +73,23 @@ extension FirstViewController: UITabBarControllerDelegate{
             return true
         }
     }
+}
+
+extension FirstViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 1
+        return posts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // 2
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell")!
+        
+        cell.textLabel!.text = "Post"
+        
+        return cell
+    }
+    
 }
 
